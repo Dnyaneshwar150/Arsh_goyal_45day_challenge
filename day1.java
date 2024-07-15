3c42b8bd810ad91c8c794d86495fa701be4b802a
Dnyaneshwar150 <dnyaneshwardimble25436@gmail.com>
2024-07-15 23:30:57 +0530

    add one more

--- day1.java
+++ day1.java
@@ -47,4 +47,32 @@ public int findDuplicate(int[] nums) {
 
     }
 
+    class Solution {
+        void swap(int nums[],int a,int b){
+        int temp  = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+            
+            
+        }
+        public void sortColors(int[] nums) {
+            int low =0;
+            int mid = 0;
+            int high = nums.length-1;
+            
+            while(mid <= high){
+            if(nums[mid] == 0){
+            swap(nums,mid,low);
+                low++;
+                mid++;
+            }else if(nums[mid] == 1){
+            mid++;
+            }else{
+            swap(nums,mid,high);
+                high--;
+            }
+            }
+        }
+    }
+
 
